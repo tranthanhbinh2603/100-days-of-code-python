c8ca4b7c83249f8ebc15b1ce7df0976466786b82
Tran Thanh Binh <tranthanhbinh.thcsthanhson@gmail.com>
2022-08-09 20:08:49 +0700

    Commit ngày 9/8/2022 lần 1

--- Day 016/main.py
+++ Day 016/main.py
@@ -1,3 +1,30 @@
 #procedural programing
 
 #How to use OOP
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# for i in range(1,361):
+#     timmy.forward(30)
+#     timmy.left(i)
+# for i in range(1,361):
+#     timmy.forward(30)
+#     timmy.right(i)
+#
+# my_screen = Screen()
+# print(my_screen.canvwidth) #Attribute
+# my_screen.exitonclick() #method
+
+#Ex 2:
+##install package: pip install PrettyTable
+##Or File -> Setting -> Project... -> Python Interpreter -> Dấu cộng -> Gọi thư viện lên cài.
+from prettytable import PrettyTable
+#right click -> Go To -> Implentient
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
+
