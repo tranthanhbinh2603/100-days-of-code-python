3f90bbad280b2711949bfcd34949a72f623a5125
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2022-11-09 23:12:37 +0700

    Commit ngày 9/11/2022 lần 1

--- Day 039 + 040/test.py
+++ Day 039 + 040/test.py
@@ -1,12 +1,14 @@
-n = int(input('Nhap so sao toi da: '))
-strtemp = ''
-for i in range(1,n+1):
-    for z in range(1,i+1):
-        strtemp += '*'
-    print(f'{strtemp}')
-    strtemp = ''
-for i in range(n-1, 0, -1):
-    for z in range(1,i+1):
-        strtemp += '*'
-    print(f'{strtemp}')
-    strtemp = ''
+def isPrime(n):
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+        return True
+
+a = int(input())
+b = 0
+isOn = True
+while isOn:
+    b+=1
+    if isPrime(a + b):
+        print(a+b)
+        isOn=False
